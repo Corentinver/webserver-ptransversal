656793e673a902828cf759e4ee239baf5c2bee68
Corentinver <vergnon.corentin@gmail.com>
2020-12-09 12:08:56 +0100

    initializing project

---
+++ src/main/java/cpe/ws/ptr/WebserverPtransversalApplication.java
@@ -0,0 +1,13 @@
+package cpe.ws.ptr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebserverPtransversalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebserverPtransversalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cpe/ws/ptr/WebserverPtransversalApplicationTests.java
@@ -0,0 +1,13 @@
+package cpe.ws.ptr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebserverPtransversalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
