fbf41af3e5a07408eb94bf02455a8fd747008219
Gouyq <55273887+Gouyq@users.noreply.github.com>
2021-01-12 00:00:52 +0100

    Update route controller

--- src/main/java/controller/FireController.java
+++ src/main/java/controller/FireController.java
@@ -1,18 +1,33 @@
 package controller;
 
+import javax.jms.ConnectionFactory;
+import javax.jms.Queue;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.config.JmsListenerContainerFactory;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import dto.FireDTO;
+import dto.OperationDTO;
 import repository.FireRepository;
+import repository.OperationRepository;
 import service.PasserelleService;
 import service.SocketService;
 
 @RestController
+@EnableJms
 public class FireController {
-
+	
 	@Autowired
 	public SocketService socketService;
 	
@@ -21,12 +36,40 @@ public class FireController {
 
 	@Autowired
 	public FireRepository fireRepository;
+
+	@Autowired
+	public OperationRepository operationRepository;
+
+	@Autowired
+	public JmsTemplate jmsTemplate;
+
+	@Autowired
+	public Queue queueFire;
 	
 	@PostMapping("/newFire")
 	public void newFire(@RequestBody FireDTO fire) {
-		passerelleService.newFire(fire);
-		socketService.sendNewFire(fire);
-		fireRepository.save(fire);
+		//passerelleService.newFire(fire);
+		//socketService.sendNewFire(fire);
+		FireDTO savedFire = fireRepository.save(fire);
+		jmsTemplate.convertAndSend(queueFire, savedFire);
+	}
+
+
+	@PostMapping("/updateFire")
+	public void updateFire(@RequestBody FireDTO fire) {
+		//passerelleService.newFire(fire);
+		//socketService.sendNewFire(fire);
+		FireDTO savedFire = fireRepository.save(fire);
+		jmsTemplate.convertAndSend(queueFire, savedFire);
+	}
+
+
+	@PostMapping("/newOperation")
+	public void newOperation(@RequestBody OperationDTO operation) {
+		//passerelleService.newFire(fire);
+		//socketService.sendNewFire(fire);
+		FireDTO savedFire = OperationRepository.save(fire);
+		jmsTemplate.convertAndSend(queueFire, savedFire);
 	}
 
 }
--- src/main/java/controller/ResourceController.java
+++ src/main/java/controller/ResourceController.java
@@ -11,7 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import dto.FireFighterDTO;
 import dto.FireStationDTO;
+import dto.J_TVehicle_TFireDTO;
 import dto.SensorDTO;
+import dto.TypeFireDTO;
 import dto.VehicleDTO;
 import service.ResourceService;
 
@@ -43,5 +45,19 @@ public class ResourceController {
 	public List<SensorDTO> allSensor()
 	{
 		return resourceService.getAllSensor();
+	}
+	
+	@GetMapping("/allTypeFire") 
+	public List<TypeFireDTO> allTypeFire()
+	{
+		return resourceService.getAllTypeFire();
+	}
+
+
+	@GetMapping("/allTVehicle_TFire") 
+	public List<J_TVehicle_TFireDTO> allTVehicle_TFire()
+	{
+		System.out.println(resourceService.getAllTVehicle_TFire());
+		return resourceService.getAllTVehicle_TFire();
 	}
 }
