0799c6fb2c27de93e1ca44ac295a5a538d3e0442
Corentinver <vergnon.corentin@gmail.com>
2021-01-12 10:18:57 +0100

    add dto for ride

--- src/main/java/cpe/ws/ptr/WebserverPtransversalApplication.java
+++ src/main/java/cpe/ws/ptr/WebserverPtransversalApplication.java
@@ -17,6 +17,7 @@ import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import dto.internal.FireDTO;
+import dto.internal.PointDTO;
 
 @SpringBootApplication
 @EnableWebMvc
@@ -34,18 +35,29 @@ public class WebserverPtransversalApplication {
 	@Bean
 	public CommandLineRunner websocketDemo() {
 		return (args) -> {
-			/*
+			
 			while (true) {
 				try {
-					Thread.sleep(3*1000); // Each 3 sec.
-					messagingTemplate.convertAndSend("/newFire", new FireDTO());
-					messagingTemplate.convertAndSend("/newFire/test", new FireDTO());
-					messagingTemplate.convertAndSend("/operation", new FireDTO());
+					Thread.sleep(15000);
+					FireDTO fire = new FireDTO();
+					fire.setId("fire1");
+					fire.setIntensity(80);
+					fire.setLocation(new PointDTO(45.8027129, 4.7923363));
+					messagingTemplate.convertAndSend("/newFire", fire);
+					System.out.println("Envoi");
+					Thread.sleep(10000);
+					fire.setIntensity(10);
+					messagingTemplate.convertAndSend("/updateFire", fire);
+					System.out.println("Update");
+					Thread.sleep(10000);
+					fire.setIntensity(0);
+					messagingTemplate.convertAndSend("/updateFire", fire);
+					System.out.println("Update");
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
-			*/
+			
 		};
 	}
 }
---
+++ src/main/java/dto/internal/RideDTO.java
@@ -0,0 +1,49 @@
+package dto.internal;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document("Ride")
+public class RideDTO {
+
+	public String operationId;
+	public String vehicleId;
+	public List<PointDTO> listLocalisations;
+	public double duration;
+	
+	public RideDTO() {}
+
+	public String getOperationId() {
+		return operationId;
+	}
+
+	public void setOperationId(String operationId) {
+		this.operationId = operationId;
+	}
+
+	public String getVehicleId() {
+		return vehicleId;
+	}
+
+	public void setVehicleId(String vehicleId) {
+		this.vehicleId = vehicleId;
+	}
+
+	public List<PointDTO> getListLocalisations() {
+		return listLocalisations;
+	}
+
+	public void setListLocalisations(List<PointDTO> listLocalisations) {
+		this.listLocalisations = listLocalisations;
+	}
+
+	public double getDuration() {
+		return duration;
+	}
+
+	public void setDuration(double duration) {
+		this.duration = duration;
+	}
+	
+}
